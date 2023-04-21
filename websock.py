eaf3ac22db47a2470c81c54fee9810532a62a2c4
Michał <michakomoro@gmail.com>
2023-04-21 05:31:51 +0200

    Refactoring

--- websock.py
+++ websock.py
@@ -6,47 +6,47 @@ import numpy as np
 import websocket
 import json
 from discord_webhook import DiscordWebhook
-
 from config import WEBHOOK_URL
 
-VOLUME_ARRAY = []
-CLOSE_ARRAY = []
+volume_array = []
+close_array = []
 cross = False
+
 
 def on_message(ws, message):
     data = json.loads(message)
-    candle = data['k']
+    candle = data["k"]
 
-    if candle['x']:
-        volume = float(candle['v'])
-        close = float(candle['c'])
-        VOLUME_ARRAY.append(volume)
-        CLOSE_ARRAY.append(close)
+    if candle["x"]:
+        volume = float(candle["v"])
+        close = float(candle["c"])
+        volume_array.append(volume)
+        close_array.append(close)
 
         # msg = f"{pd.to_datetime(candle['t'], unit='ms')} volume = {volume} ARB"
 
         print(f"Candle opened at {pd.to_datetime(candle['t'], unit='ms')} has closed.")
         print(f"Volume = {volume}")
 
-        # if len(VOLUME_ARRAY) > 20 or len(CLOSE_ARRAY) > 14:
-        #     rvol = volume / np.array(VOLUME_ARRAY)[-20:].mean()
+        # if len(volume_array) > 20 or len(close_array) > 14:
+        #     rvol = volume / np.array(volume_array)[-20:].mean()
         #     if rvol > 2:
         #         msg = f"{pd.to_datetime(candle['t'], unit='ms')} volume = {volume} ARB, RVOL = {rvol}"
         #         webhook = DiscordWebhook(url=WEBHOOK_URL, content=msg)
         #         response = webhook.execute()
 
-        if len(CLOSE_ARRAY) > 14:
-            rsi_val = pd.DataFrame({"close": CLOSE_ARRAY}).ta.rsi().values[-1]
+        if len(close_array) > 14:
+            rsi_val = pd.DataFrame({"close": close_array}).ta.rsi().values[-1]
             print(f"RSI = {np.round(rsi_val):.4f}")
 
-            if (rsi_val > 70 or rsi_val < 30) and cross == False:
+            if not cross and (rsi_val < 30 or rsi_val > 70):
                 cross = True
                 msg = f"{pd.to_datetime(candle['t'], unit='ms')}, RSI = {rsi_val}"
                 webhook = DiscordWebhook(url=WEBHOOK_URL, content=msg)
                 response = webhook.execute()
-            if rsi_val >= 30 and rsi_val <= 70:
+            if 30 <= rsi_val <= 70:
                 cross = False
-        
+
         print("")
         time.sleep(58)
 
